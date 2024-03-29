2ebe15a721d4ad968d0242ac6fa9c92ede212864
Farming_Assistance_Website <sushantnamurte@gmail.com>
2024-03-29 20:29:49 +0530

    Nice WEb

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Banner1 from "./components/Banner1";
+
+
+function App() {
+  return (<>
+  <Navbar/>
+  <Banner1/>
+
+  
+  </>
+  );
+}
+
+export default App;
